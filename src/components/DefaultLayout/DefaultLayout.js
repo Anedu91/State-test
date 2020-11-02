f19e92f65c4847b045722663b75be8728fabf024
Angel <diaz.angeledu@gmail.com>
2020-11-02 17:35:58 -0400

    letter

--- src/components/DefaultLayout/DefaultLayout.js
+++ src/components/DefaultLayout/DefaultLayout.js
@@ -3,7 +3,7 @@ import React from "react";
 function DefaultLayout({ children }) {
   return (
     <>
-      <h1 className="text-6xl my-5 text-center">This is a state test</h1>
+      <h1 className="text-6xl my-5 text-center">This is an state test</h1>
       <div className="container grid mx-auto grid-cols-3 gap-4 pt-10">
         {children}
       </div>
